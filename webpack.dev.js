4836602cf79dc46498f912c2b9ab37222169d0b1
kwu <kwu@riotgames.com>
2018-05-27 01:40:03 -0700

    Added docs back and updated local build.

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './docs',
+    contentBase: './build',
   },
   mode: 'development',
 })
